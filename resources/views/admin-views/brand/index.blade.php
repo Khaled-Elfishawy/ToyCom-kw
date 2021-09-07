f42e87e3a1caa1dad7602f4cbe521ee5e11dd4b6
Khaled Elfishawy <khalidelfishawy@gmail.com>
2021-09-07 23:12:45 +0200

    Brands Images

--- resources/views/admin-views/brand/index.blade.php
+++ resources/views/admin-views/brand/index.blade.php
@@ -62,7 +62,7 @@
                                     </span>
                                     </td>
                                     <td>{{$brand->name_en}}</td>
-                                    <td><img src="{{asset('storage/app/public/product')}}/{{$brand->image }}" alt="iamge"  width="70px" height="70px" style="border-radius: 50%"></td>
+                                    <td><img src="{{asset('storage/app/public/brand')}}/{{$brand->image }}" alt="iamge"  width="70px" height="70px" style="border-radius: 50%"></td>
                                     <td>
                                         <!-- Dropdown -->
                                         <div class="dropdown">
