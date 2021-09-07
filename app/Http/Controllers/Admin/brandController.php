044a718bfe4eb60f7ffe4e5dbac25db5db778985
Khaled Elfishawy <khalidelfishawy@gmail.com>
2021-09-07 22:21:54 +0200

    Update brandController.php

--- app/Http/Controllers/Admin/brandController.php
+++ app/Http/Controllers/Admin/brandController.php
@@ -40,13 +40,12 @@ class brandController extends Controller
     public function store(Request $request)
     {
         $input = $request->all();
-        dd($input);
-
-        if (!empty($request->file('images'))) {
+        if (!empty($request->file('image'))) {
             $image_data =  Helpers::upload('brands/', 'png', $img);
         } else {
             $image_data = '';
         }
+        dd($image_data);
         $file_name = $this->saveImage($request->file('image'), 'brand/img' );
         $input['image'] = $file_name;
         Brand::Create($input);
