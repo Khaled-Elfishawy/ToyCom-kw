6638d01fcad7df3d4436c58a8a056929b12c323d
Khaled Elfishawy <khalidelfishawy@gmail.com>
2021-09-13 16:24:48 +0200

    Update CustomerAuthController.php

--- app/Http/Controllers/Api/V1/CustomerAuthController.php
+++ app/Http/Controllers/Api/V1/CustomerAuthController.php
@@ -23,15 +23,29 @@ class CustomerAuthController extends Controller
         if ($validator->fails()) {
             return response()->json(['errors' => Helpers::error_processor($validator)], 403);
         }
-        // to make calculation to get mony ..
-        $points_dinar = BusinessSetting::where('key', 'points_dinar')->first()->value;
-        $money = $request->points / $points_dinar;    //ex.  700/100 = 7 dinar
-        //to save data to user table
         $user = User::find($request->user()->id);
-        $user->my_points = $user->my_points - $request->points;  //ex.  700 - 700 = 0 points
-        $user->my_money = $user->my_money + $money;  // ex.  0 + 7 = 7 dinar
-        $user->save();
-        return response()->json(true, 200);
+        if ($user->my_points >= $request->points) {
+            $points_dinar = BusinessSetting::where('key', 'points_dinar')->first()->value;
+            $money = $request->points / $points_dinar;    //ex.  700/100 = 7 dinar
+            $user->my_points = $user->my_points - $request->points;  //ex.  700 - 700 = 0 points
+            $user->my_money = $user->my_money + $money;  // ex.  0 + 7 = 7 dinar
+            $user->save();
+            //New Array
+            $dinar_points = BusinessSetting::where('key', 'dinar_points')->first()->value;
+            $points_dinar = BusinessSetting::where('key', 'points_dinar')->first()->value;
+            $user = User::find($request->user()->id);
+            $user->my_points;
+            $user->my_money;
+            $array = [
+                'user_point'     => $user->my_points,
+                'user_wallet'    => $user->my_money,
+                'points_dinar'   => floatval($points_dinar)
+            ];
+            return response()->json($array, 200);
+        }else{
+            return response()->json(false, 200);
+        }
+        
     }
     public function points_info(Request $request)
     {
