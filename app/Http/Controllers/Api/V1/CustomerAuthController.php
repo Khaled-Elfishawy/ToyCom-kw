bc8c5cc9c59c7e6ef0a2dcd0f6166291fa9ce200
Khaled Elfishawy <khalidelfishawy@gmail.com>
2021-09-13 15:12:24 +0200

    Update CustomerAuthController.php

--- app/Http/Controllers/Api/V1/CustomerAuthController.php
+++ app/Http/Controllers/Api/V1/CustomerAuthController.php
@@ -43,7 +43,7 @@ class CustomerAuthController extends Controller
         $array = [
             'user_point'     => $user->my_points,
             'user_wallet'    => $user->my_money,
-            'dinar_to_points'   => $dinar_points,
+            'dinar_to_points'=> floatval($dinar_points),
         ];
         return response()->json($array, 200);
     }
