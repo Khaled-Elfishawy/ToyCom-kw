51e0c936dd0339dabeac408ddec793e7f40129ce
Khaled Elfishawy <khalidelfishawy@gmail.com>
2021-11-08 17:04:31 +0200

    Create WalletController.php

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\BusinessSetting;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+
+class WalletController extends Controller
+{
+    public function pay($value='')
+    {
+        // code...
+    }
+}
