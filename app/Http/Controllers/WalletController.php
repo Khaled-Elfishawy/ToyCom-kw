0c662a1505f5cc09bfe3e51f4dfb3afc1d6eb2da
Khaled Elfishawy <khalidelfishawy@gmail.com>
2021-11-08 17:11:56 +0200

    Update WalletController.php

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -6,11 +6,16 @@ use App\Model\BusinessSetting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
+use App\Model\Order;
+use App\User;
 
 class WalletController extends Controller
 {
-    public function pay($value='')
+    public function pay(Request $request)
     {
-        // code...
+        $order = Order::with(['details'])->where(['id' => session('order_id')])->first();
+        $user  = User::find($order->user_id);
+        return dd($user);
+
     }
 }
