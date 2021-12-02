47411ead2fac0fd6390df4d5a87027721e026097
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-12-02 17:50:36 +0200

    fix brans api v2

--- app/Http/Controllers/Api/V1/BannerController.php
+++ app/Http/Controllers/Api/V1/BannerController.php
@@ -56,9 +56,9 @@ class BannerController extends Controller
                 "created_at" => "2021-08-22 15:50:38",
                 "updated_at" => "2021-09-06 10:18:15",
             ];
-            $barnds = Brand::all();
-            $barnds->prepend($defualt);
-            return response()->json($barnds->orderBy('order_num','asc'), 200);
+            $brands = Brand::all();
+            $brands->prepend($defualt);
+            return response()->json($brands->sortBy('order_num'), 200);
         } catch (\Exception $e) {
             return response()->json([], 200);
         }
